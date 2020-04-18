4d588c8124740a0a09800d3f4088e4a64173f8b9
ManarElkady <manaraelkady@gmail.com>
2020-04-18 10:00:08 -0700

    Add Network Layer for League, Team, Sports with parsing

--- SportyNews/Model/Constants/APIURLs.swift
+++ SportyNews/Model/Constants/APIURLs.swift
@@ -2,18 +2,27 @@
 //  APIURLs.swift
 //  SportyNews
 //
-//  Created by Esraa Hassan on 4/18/20.
+//  Created by Manar on 4/18/20.
 //  Copyright © 2020 ITI. All rights reserved.
 //
 
 import Foundation
 class APIURLs {
+    /****** Strings URL******/
     static let sportString = "https://www.thesportsdb.com/api/v1/json/1/all_sports.php"
     static let leagueSearchString = "https://www.thesportsdb.com/api/v1/json/1/search_all_leagues.php?s="
+    static let teamSearchLeague = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l="
+    
+    /**Keys**/
+    static var searchLeagueKey : String?
+    static var searchTeamKey : String?
+    
+    /****** URLs******/
     static let sportsURL : URL = URL(string: sportString)!
-    static var searchLeagueKey: String?
     
     static let searchLeagueURL : URL = URL(string: "\(leagueSearchString)\(searchLeagueKey!)")!
+    
+    static let searchTeamLeagueURL : URL = URL(string: "\(teamSearchLeague)\(searchTeamKey!)")!
     
 }
 
