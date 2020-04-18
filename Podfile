52429671fe1bd5048500f97aafb594ae37c2fd55
AhmedIbrahem <ahmed.ibrahem@fci.helwan.edu.eg>
2020-04-18 05:58:40 -0700

    init commit

---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'SportyNews' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SportyNews
+
+pod 'Alamofire', '~> 4.2.0'
+pod 'Kingfisher', '~> 4.7.0'
+pod 'SwiftyJSON', '~> 4.2.0'
+pod "YoutubePlayer-in-WKWebView", "~> 0.2.0"
+pod 'RxSwift', '~> 4.2.0'
+
+end
---
+++ SportyNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportyNews
+//
+//  Created by Esraa Hassan on 4/17/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
