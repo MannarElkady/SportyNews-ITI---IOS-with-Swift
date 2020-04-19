98ba89ee6fc2305b1470619a013f843fcc061f33
AhmedIbrahem <ahmed.ibrahem@fci.helwan.edu.eg>
2020-04-19 07:51:33 -0700

    add TeamDetails View

---
+++ SportyNews/TeamDetails/TeamDetailsProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  TeamProtocol.swift
+//  SportyNews
+//
+//  Created by Esraa Hassan on 4/19/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import Foundation
---
+++ SportyNews/TeamDetails/TeamDetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TeamDetailsViewController.swift
+//  SportyNews
+//
+//  Created by Esraa Hassan on 4/19/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import UIKit
+
+class TeamDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.jpeg")!)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
