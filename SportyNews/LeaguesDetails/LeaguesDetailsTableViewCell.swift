802f9cc3149ac61c80147c4772f29de749e64f6d
AhmedIbrahem <ahmed.ibrahem@fci.helwan.edu.eg>
2020-04-19 12:29:21 -0700

    add LeaguesDetailsTableViewCell

---
+++ SportyNews/LeaguesDetails/LeaguesDetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LeaguesDetailsTableViewCell.swift
+//  SportyNews
+//
+//  Created by Esraa Hassan on 4/19/20.
+//  Copyright © 2020 ITI. All rights reserved.
+//
+
+import UIKit
+
+class LeaguesDetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
